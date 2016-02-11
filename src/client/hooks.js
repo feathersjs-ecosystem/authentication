48225113bbfaa85f6dc2afa312e6253f64b56bdc
Eric Kryski <e.kryski@gmail.com>
2016-02-11 02:10:50 -0700

    Fixing client side hooks

--- src/client/hooks.js
+++ src/client/hooks.js
@@ -1,26 +1,34 @@
 import utils from './utils';
 
-export let populateParams = function(options = {}) {
+export let populateParams = function() {
   return function(hook) {
     hook.params.user = utils.getUser();
     hook.params.token = utils.getToken();
   };
 };
 
-export let populateHeader = function(options = {header: 'Authorization'}) {
+export let populateHeader = function(options = {}) {
+  const defaults = {
+    header: 'Authorization'
+  };
+  
+  options = Object.assign({}, defaults, options);
+
   return function(hook) {
-    hook.params.headers = {
-      [options.header]: hook.params.token
+    if (hook.params.token) {
+      hook.params.headers = {
+        [options.header]: hook.params.token
+      }; 
     }
-  }
+  };
 };
 
-export let populateSocketParams = function(options = {}) {
+export let populateSocketParams = function() {
   return function(hook) {
     if (hook.params.token) {
       hook.params.query = {
         token: hook.params.token
-      }  
+      };
     }
   };
 };
@@ -29,4 +37,4 @@ export default {
   populateParams,
   populateHeader,
   populateSocketParams
-}
+};
