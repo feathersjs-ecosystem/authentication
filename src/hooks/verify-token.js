0c0c97a3998600eea35b1a8ed4ee4d547c78d85b
Eric Kryski <e.kryski@gmail.com>
2016-02-19 14:54:25 -0700

    Fixing verifyToken hook so that you don't have to pass a secret if you configured things correctly. Closes #69

--- src/hooks/verify-token.js
+++ src/hooks/verify-token.js
@@ -10,16 +10,23 @@ import errors from 'feathers-errors';
 export default function(options = {}){
   const secret = options.secret;
 
-  if (!secret) {
-    console.log('no secret', options);
-    throw new Error('You need to pass `options.secret` to the verifyToken() hook.');
-  }
-
   return function(hook) {
     const token = hook.params.token;
 
     if (!token) {
       return Promise.resolve(hook);
+    }
+
+    if (!secret) {
+      // Try to get the secret from the app config
+      const authOptions = app.get('auth');
+
+      if (authOptions && authOptions.token && authOptions.token.secret) {
+        secret = authOptions.token.secret;
+      }
+      else {
+        throw new Error('You need to pass `options.secret` to the verifyToken() hook or set `auth.token.secret` it in your config.');
+      }
     }
 
     return new Promise(function(resolve, reject){
