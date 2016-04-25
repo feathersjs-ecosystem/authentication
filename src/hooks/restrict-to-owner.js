1ddf4ca409a0e2a1e707a15ac4028a20bb054428
Tobe O <thosakwe@gmail.com>
2016-04-25 16:27:29 -0400

    Update restrict-to-owner.js
    https://github.com/feathersjs/feathers-authentication/issues/175

--- src/hooks/restrict-to-owner.js
+++ src/hooks/restrict-to-owner.js
@@ -12,7 +12,8 @@ export default function(options = {}){
       throw new Error(`The 'restrictToOwner' hook should only be used as a 'before' hook.`);
     }
 
-    if (!hook.id) {
+    const methodIsAllowed = hook.method === 'get' || hook.method === 'update' || hook.method === 'patch' || hook.method == 'remove';
+    if (!methodIsAllowed) {
       throw new errors.MethodNotAllowed(`The 'restrictToOwner' hook should only be used on the 'get', 'update', 'patch' and 'remove' service methods.`);
     }
 
