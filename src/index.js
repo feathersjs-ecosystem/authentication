e6d4a8630ccb3d1c2bd3b9ccc2677e594d0019ff
Eric Kryski <e.kryski@gmail.com>
2016-07-18 00:13:18 -0600

    removing auth redirects and exposing middleware

--- src/index.js
+++ src/index.js
@@ -21,10 +21,8 @@ const PROVIDERS = {
 // Options that apply to any provider
 const defaults = {
   idField: '_id',
-  shouldSetupSuccessRoute: true,
-  shouldSetupFailureRoute: true,
-  successRedirect: '/auth/success',
-  failureRedirect: '/auth/failure',
+  // successRedirect: '/auth/success',
+  // failureRedirect: '/auth/failure',
   tokenEndpoint: '/auth/token',
   localEndpoint: '/auth/local',
   userEndpoint: '/users',
@@ -63,18 +61,6 @@ export default function auth(config = {}) {
     // Merge and flatten options
     const authOptions = Object.assign({}, defaults, app.get('auth'),config);
 
-    // If a custom success redirect is passed in or it is disabled then we
-    // won't setup the default route handler.
-    if (authOptions.successRedirect !== defaults.successRedirect) {
-      authOptions.shouldSetupSuccessRoute = false;
-    }
-
-    // If a custom failure redirect is passed in or it is disabled then we
-    // won't setup the default route handler.
-    if (authOptions.failureRedirect !== defaults.failureRedirect) {
-      authOptions.shouldSetupFailureRoute = false;
-    }
-
     // Set the options on the app
     app.set('auth', authOptions);
 
@@ -85,7 +71,7 @@ export default function auth(config = {}) {
       // app.use( middleware.exposeAuthenticatedUser() );
 
       // Get the token and expose it to REST services.
-      app.use( middleware.normalizeAuthToken(authOptions) );
+      app.use( middleware.tokenParser(authOptions) );
     }
 
     app.use(passport.initialize());
@@ -144,26 +130,9 @@ export default function auth(config = {}) {
 
     // Register error handling middleware for redirecting to support
     // redirecting on authentication failure.
-    app.use(middleware.failedLogin(authOptions));
-
-    // Setup route handler for default success redirect
-    if (authOptions.shouldSetupSuccessRoute) {
-      debug(`Setting up successRedirect route: ${authOptions.successRedirect}`);
-
-      app.get(authOptions.successRedirect, function(req, res){
-        res.sendFile(path.resolve(__dirname, 'public', 'auth-success.html'));
-      });
-    }
-
-    // Setup route handler for default failure redirect
-    if (authOptions.shouldSetupFailureRoute) {
-      debug(`Setting up failureRedirect route: ${authOptions.failureRedirect}`);
-
-      app.get(authOptions.failureRedirect, function(req, res){
-        res.sendFile(path.resolve(__dirname, 'public', 'auth-fail.html'));
-      });
-    }
+    // app.use(middleware.failedLogin(authOptions));
   };
 }
 
 auth.hooks = hooks;
+auth.middleware = middleware;
