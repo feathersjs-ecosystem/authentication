0590f5696c03bb660748156095b47e0064121fb7
francisco-sanchez-molina <psm1984@gmail.com>
2017-02-13 10:12:07 +0100

    Refresh token must have a user ID

--- src/services/token.js
+++ src/services/token.js
@@ -81,7 +81,9 @@ export class Service {
     }
 
     const options = this.options;
-    const data = params;
+    const data = {
+        _id: params.data._id
+    };
     // Our before hook determined that we had a valid token or that this
     // was internally called so let's generate a new token with the user
     // id and return both the ID and the token.
