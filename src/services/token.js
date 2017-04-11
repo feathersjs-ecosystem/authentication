d34788c34510cd5bdfba7fb9f139d4ccf3de02af
francisco-sanchez-molina <psm1984@gmail.com>
2017-04-11 08:49:53 +0200

    Update token.js

--- src/services/token.js
+++ src/services/token.js
@@ -81,9 +81,8 @@ export class Service {
     }
 
     const options = this.options;
-    const data = {
-        _id: params.data._id
-    };
+    const data = params.data;
+    
     // Our before hook determined that we had a valid token or that this
     // was internally called so let's generate a new token with the user
     // id and return both the ID and the token.
